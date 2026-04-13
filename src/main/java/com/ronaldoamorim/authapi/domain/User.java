dbd023c5416f987e6ed77d8a22393e529cbde5a9
Ronaldo Amorim <ronaldop.amorim@hotmail.com>
2026-04-13 20:52:51 -0300

    feat: implements UserDetails for the User entity

--- src/main/java/com/ronaldoamorim/authapi/domain/User.java
+++ src/main/java/com/ronaldoamorim/authapi/domain/User.java
@@ -3,16 +3,23 @@ package com.ronaldoamorim.authapi.domain;
 import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Table(name = "users")
 @Entity(name = "users")
 @Getter
 @Setter
+@NoArgsConstructor
 @EqualsAndHashCode(of = "id")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -22,5 +29,22 @@ public class User {
 
     private String password;
 
+    @Enumerated(EnumType.STRING)
     private UserRole role;
+
+    public User (String login, String password, UserRole role) {
+        this.login = login;
+        this.password = password;
+        this.role = role;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
 }
